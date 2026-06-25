c40bb65913a0fbbe4b81b8e936a6fb1fae413d0b
Anamul Haque <anamulhaque4631@gmail.com>
2026-06-25 12:16:55 +0600

    server side add , post parcel data, and get parcel data

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+// Force Node.js to use Cloudflare and Google public DNS
+const dns = require("node:dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzupc.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("ans_shift_db");
+    const parcelCollection = db.collection("parcels");
+
+    // parcel api
+
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+
+      const { email } = req.query;
+
+      //  /parcels?email=""&
+      if (email) {
+        query.senderEmail = email;
+      }
+      const cursor = parcelCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // send parcel data
+
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Ans.shift");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
